12b2f3fa2a645abf58d18fc7414ec40759dc151c
Prateek Thakur <prateekthakur272@gmail.com>
2025-11-06 19:31:06 +0530

    Initial Commit

---
+++ DragonBallInfoPedia/DragonBallInfoPediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragonBallInfoPediaApp.swift
+//  DragonBallInfoPedia
+//
+//  Created by Prateek on 06/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DragonBallInfoPediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
